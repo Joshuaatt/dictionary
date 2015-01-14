9aedf84ceb7b426d036d36eb83651b23e279fbbc
Andy Uppendahl & Joshua Atteberry <student@epicodus.com>
2015-01-14 14:19:47 -0800

    Add Word and Definition class definition files along with related spec files.

--- lib/term.rb
+++ lib/term.rb
@@ -1,6 +1,6 @@
 class Term
-
-  attr_accessor :word, :definition
+  attr_reader :definition
+  attr_accessor :word
 
   @@all_terms = []
 
@@ -19,6 +19,20 @@ class Term
 
   define_method(:save) do
     @@all_terms.push(self)
+  end
+
+  define_singleton_method(:search) do |search_word|
+    found_term = nil
+    @@all_terms.each() do |term|
+      if term.word() == search_word
+        return found_term = term
+      end
+    end
+    found_term
+  end
+
+  define_method(:add_definition) do |new_def|
+    @definition.push(new_def)
   end
 
 end
--- spec/term_spec.rb
+++ spec/term_spec.rb
@@ -4,15 +4,15 @@ require("term")
 describe(Term) do
   describe("#word") do
     it("takes in the word for a newly created Term object, and returns the word") do
-      test_term = Term.new("coffee", "a delicious caffeinated drink")
+      test_term = Term.new("coffee", ["a delicious caffeinated drink"])
       expect(test_term.word()).to(eq("coffee"))
     end
   end
 
   describe("#definition") do
     it("takes in a definition for a newly created term object and returns the definition") do
-      test_term = Term.new("coffee", "a delicious caffeinated drink")
-      expect(test_term.definition()).to(eq("a delicious caffeinated drink"))
+      test_term = Term.new("coffee", ["a delicious caffeinated drink"])
+      expect(test_term.definition()).to(eq(["a delicious caffeinated drink"]))
     end
   end
 
@@ -24,7 +24,7 @@ describe(Term) do
 
   describe("#save") do
     it("adds a Term to the Term class array of all saved terms") do
-      test_term = Term.new("game", "a fun entertainment activity")
+      test_term = Term.new("game", ["a fun entertainment activity"])
       test_term.save()
       expect(Term.all()).to(eq([test_term]))
     end
@@ -32,9 +32,36 @@ describe(Term) do
 
   describe(".clear") do
     it("empties out all saved terms from the Term class array") do
-      Term.new("game", "a fun entertainment activity")
+      Term.new("game", ["a fun entertainment activity"])
       Term.clear()
       expect(Term.all()).to(eq([]))
     end
   end
+
+  describe(".search") do
+    it("takes a word and returns the term if found") do
+      test_term_1 = Term.new("coffee", ["a delicious caffeinated drink"])
+      test_term_1.save()
+      test_term_2 = Term.new("game", ["a fun entertainment activity"])
+      test_term_2.save()
+      expect(Term.search("coffee")).to(eq(test_term_1))
+    end
+
+    it("takes a word and returns nil if the term if found") do
+      test_term_1 = Term.new("coffee", ["a delicious caffeinated drink"])
+      test_term_1.save()
+      test_term_2 = Term.new("game", ["a fun entertainment activity"])
+      test_term_2.save()
+      expect(Term.search("bottle")).to(eq(nil))
+    end
+  end
+
+  describe("#add_definition") do
+    it("adds a definition to an existing term") do
+      term = Term.new("coffee", ["a drink"])
+      term.save()
+      expect(term.add_definition("a drug")).to(eq(["a drink", "a drug"]))
+    end
+  end
+
 end
